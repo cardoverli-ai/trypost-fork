08c557bb7e6d7aacb29aba38ae7c4de2d5f083af
Paulo Castellano <paulo@castellanos.llc>
2026-06-22 17:22:46 -0300

    style(sidebar): condense the gap between nav groups (gap-2 to gap-0.5)

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -232,7 +232,7 @@ const handleCreateWorkspace = () => {
             </SidebarMenu>
         </SidebarHeader>
 
-        <SidebarContent>
+        <SidebarContent class="gap-0.5">
             <!-- Create Post Button -->
             <div v-if="currentWorkspace && canCreatePost" class="px-2 py-2">
                 <Link :href="createPost.url()" class="block">
